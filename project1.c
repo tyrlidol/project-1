500febfbc597709c753016888f350268cebb02ee
tyrlidol <48045480+tyrlidol@users.noreply.github.com>
2019-04-10 18:48:27 +1000

    update wasn't commiting properly

--- project1.c
+++ project1.c
@@ -1,12 +1,40 @@
-    /*project 1 ciphers*/
-
+   /*project 1 ciphers*///
+//rotation cipher
 #include<stdio.h>
-
-main()
+int main()
 {
-    float X;
-    printf("Enter text for encryption: ");
-    scanf("%f", &X);
+char code[100], ch;
+int i, key=3;
+//scanf("%s", code );
+scanf("%[^\n]%*c",code);
+printf(" code to encrypt: %s \n", code);
+printf(" key: ");
+scanf("%d", &key);
+	
+	for(i = 0; code[i] != '\0'; ++i){
+		ch = code[i];
+		
+		if(ch >= 'a' && ch <= 'z'){
+			ch = ch + key;
+			
+			if(ch > 'z'){
+				ch = ch - 'z' + 'a' - 1;
+			}
+			
+			code[i] = ch;
+		}
+		else if(ch >= 'A' && ch <= 'Z'){
+			ch = ch + key;
+			
+			if(ch > 'Z'){
+				ch = ch - 'Z' + 'A' - 1;
+			}
+			
+			code[i] = ch;
+		}
+	}
+	
+	printf("Encrypted code: %s ", code);
 
 
 }
